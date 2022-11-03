9610413d63e745da820c76a3c3c30b0eda10e657
ayesha2325 <77952410+ayesha2325@users.noreply.github.com>
2022-11-03 11:42:24 -0400

    Update fattree.py (#6)

--- emulator/fattree.py
+++ emulator/fattree.py
@@ -70,6 +70,58 @@ class ContainerManager:
                     os.system("docker exec -it pod-{}-host-{} apt install -y -q iproute2".format(i, host_id))
                     os.system("docker exec -it pod-{}-host-{} ip addr add 10.{}.{}.{} dev lo".format(i, host_id, i, j, h))
                     host_id += 1
+                    
+    def connect(self):
+
+        # Veth pairs from core switches to pod-agg switches
+        # veth links have names of the type CA-C{core_switch_id}-P{pod_id}A{agg_swicth_id} e.g CA0C0-P0A0 and CA0P0A0-C0
+        # Prefix CA in names for links between Core switches and Aggregation switches
+        # Prefix AE in names for links between Aggregation switches and Edge switches
+        # Prefix EH in names for links between Edge switches and Hosts
+
+        for i in range(int(self.k/2)):
+            for j in range(int(self.k/2)):
+                for m in range(self.k):
+                    x = i*(int(self.k/2))
+                    os.system("sudo ip link add CA-C{}-P{}A{} type veth peer name CA-P{}A{}-C{}".format(j+x,m,i, m,i, j+x))
+                    # set CA-C{}-P{}A{} up in core-{}
+                    # set CA-P{}A{}-C{} up in pod-{}-agg-{}
+                    # pid1 = sp.check_output(['docker', 'inspect', '-f', '{{.State.Pid}}', 'core-{}'.format(j+x)]).decode("utf-8").strip()
+                    # pid2 = sp.check_output(['docker', 'inspect', '-f', '{{.State.Pid}}', 'pod-{}-agg-{}'.format(m,i)]).decode("utf-8").strip()
+                    # os.system('sudo ip link set CA-C{}-P{}A{} netns {}'.format(j+x,m,i,int(pid1)))
+                    # os.system('sudo ip link set CA-P{}A{}-C{} netns {}'.format(m,i, j+x, int(pid2)))
+                    # os.system('sudo nsenter -t {} -n ip link set dev CA-C{}-P{}A{} up'.format(int(pid1), j+x,m,i))
+                    # os.system('sudo nsenter -t {} -n ip link set dev CA-P{}A{}-C{} up'.format(int(pid2), m,i, j+x))
+                   
+        # Veth pairs from agg switches to edge switches
+        
+        for i in range(self.k): 
+            for j in range(int(self.k/2)): 
+                for m in range(int(self.k/2)):
+                    os.system("sudo ip link add AE-P{}A{}-P{}E{} type veth peer name AE-P{}E{}-P{}A{}".format(i,j,i,m,i,m,i,j))
+                    # set AE-P{}A{}-P{}E{} up in pod-{}-agg-{}
+                    # set AE-P{}E{}-P{}A{} up in pod-{}-edge-{}
+                    pid1 = sp.check_output(['docker', 'inspect', '-f', '{{.State.Pid}}', 'pod-{}-agg-{}'.format(i,j)]).decode("utf-8").strip()
+                    pid2 = sp.check_output(['docker', 'inspect', '-f', '{{.State.Pid}}', 'pod-{}-edge-{}'.format(i,m)]).decode("utf-8").strip()
+                    os.system('sudo ip link set AE-P{}A{}-P{}E{} netns {}'.format(i,j,i,m,int(pid1)))
+                    os.system('sudo ip link set AE-P{}E{}-P{}A{} netns {}'.format(i,m,i,j int(pid2)))
+                    os.system('sudo nsenter -t {} -n ip link set dev AE-P{}A{}-P{}E{} up'.format(int(pid1), i,j,i,m))
+                    os.system('sudo nsenter -t {} -n ip link set dev AE-P{}E{}-P{}A{} up'.format(int(pid2), i,m,i,j))
+                
+        # Veth pairs from edeg switches to hosts
+        
+        for i in range(self.k):
+            for j in range(int(self.k/2)):
+                for m in range(int(self.k/2)):         
+                    os.system("sudo ip link add EH-P{}E{}-P{}H{} type veth peer name EH-P{}H{}-P{}E{}".format(i,j,i,m+(j*int(self.k/2)),i,m+(j*int(self.k/2)), i, j ))
+                    # set EH-P{}E{}-P{}H{} up in pod-{}-edge-{}
+                    # set EH-P{}H{}-P{}E{} up in pod-{}-host-{}
+                    pid1 = sp.check_output(['docker', 'inspect', '-f', '{{.State.Pid}}', 'pod-{}-edge-{}'.format(i,j)]).decode("utf-8").strip()
+                    pid2 = sp.check_output(['docker', 'inspect', '-f', '{{.State.Pid}}', 'pod-{}-host-{}'.format(i,m+(j*int(self.k/2)))]).decode("utf-8").strip()
+                    os.system('sudo ip link set EH-P{}E{}-P{}H{} netns {}'.format(i,j,i,m+(j*int(self.k/2)),int(pid1)))
+                    os.system('sudo ip link set EH-P{}H{}-P{}E{} netns {}'.format(i,m+(j*int(self.k/2)), i, j, int(pid2)))
+                    os.system('sudo nsenter -t {} -n ip link set dev EH-P{}E{}-P{}H{} up'.format(int(pid1), i,j,i,m+(j*int(self.k/2))))
+                    os.system('sudo nsenter -t {} -n ip link set dev EH-P{}H{}-P{}E{} up'.format(int(pid2), i,m+(j*int(self.k/2)), i, j))
             
     # WARNING: It will destroy all containers.
     def clean(self):
