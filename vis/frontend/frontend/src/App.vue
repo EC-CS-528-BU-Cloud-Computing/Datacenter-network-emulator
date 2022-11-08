b794c9422bc30048bdd785cd38e0d5b65e408648
MingjiHan <mjhan@bu.edu>
2022-11-08 16:02:17 -0500

    updates

---
+++ vis/frontend/frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <body>
+    <h1>Data Center Network Emulator</h1>
+  </body>
+  
+</template>
+
+<style>
+
+body {
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
