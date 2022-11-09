d17b97cca9a8bf7c291c629b4d36beb0dd44f91c
MingjiHan <mjhan@bu.edu>
2022-11-08 20:19:57 -0500

    Introduce visualization frontend (#7)
    * updates
    
    * update

---
+++ vis/frontend/frontend/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div :style="{ background: 'var(--color-fill-2)', padding: '28px' }" >
+    <a-layout-content>
+    <a-typography-title :heading="5">
+             About Us
+    </a-typography-title>
+    <a-typography-paragraph>
+     Team Members:  Mingji Han, Ayesha Naeem, Alex Hureaux-Perron, Muhammad Fahad Farid, Zeying Zhu
+    </a-typography-paragraph>
+  </a-layout-content>
+  </div>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ vis/frontend/frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <div :style="{ background: 'var(--color-fill-2)', padding: '28px' }" >
+    <a-layout-content>
+    <a-typography-title :heading="6">
+             This is for visualization
+      </a-typography-title>
+    </a-layout-content>
+    </div>
+  </main>
+</template>
