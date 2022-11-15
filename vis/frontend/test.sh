ad347ad6156330c8151ab5b08b33c7985b5beeb2
Mingji <mjhan@bu.edu>
2022-11-14 22:16:19 -0600

    update scripts

--- vis/frontend/test.sh
+++ vis/frontend/test.sh
@@ -6,3 +6,5 @@ for i in `ifconfig -a |grep veth |awk -F: '{print $1}'` ;
       echo "Peer ID: " `ethtool -S $i |  grep -i peer_ifindex | awk -F: '{print $2}'`
       echo "---------------"
 done
+docker exec f2ce707d306c ip link show | grep UP | grep -v eth0 | grep -v lo | awk -F: '{print $2}' | awk -F@ '{print $1}'
+docker exec f2ce707d306c ip link show | grep UP | grep -v eth0 | grep -v lo | awk -F: '{print $2}' | awk -F@ '{print $1}' | tr -d ' '
