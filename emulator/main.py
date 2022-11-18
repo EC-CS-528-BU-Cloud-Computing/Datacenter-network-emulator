0a1583066a130a88c6fe02868ab576f5e0a3f955
Mingji <mjhan@bu.edu>
2022-11-18 10:31:46 -0600

    udpates

--- emulator/main.py
+++ emulator/main.py
@@ -3,8 +3,8 @@ from fattree import ContainerManager
 if __name__ == "__main__":
     mgr = ContainerManager(4)
     mgr.clean()
-    mgr.start()
-    mgr.connect()
+   # mgr.start()
+   # mgr.connect()
     # mgr.clean()
     print("Hello FatTree")
     
