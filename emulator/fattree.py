f48c327890a346cd0abc376890a45e2c00247d04
Zeying Zhu <zeyingz@bu.edu>
2022-11-30 22:56:51 +0000

    add bridge

--- emulator/fattree.py
+++ emulator/fattree.py
@@ -84,6 +84,12 @@ class FatTree:
                     host.remove()
                 except:
                     print("didn't find pod-{}-host-{}".format(i, j))
+            
+            # bridges
+            for pod in range(0, self.k):
+                for edge in range(0, self.num_of_half_pod_sw):
+                    os.system("sudo ip link set br-p-{}-e-{} down".format(pod, edge))
+                    os.system("sudo brctl delbr br-p-{}-e-{}".format(pod, edge))
 
     def assignLoIP(self):
         # Assign loopback IPs
@@ -102,18 +108,7 @@ class FatTree:
             for j in range(0, int(self.k / 2)):
                 os.system("docker exec pod-{}-agg-{} ip addr add 15.{}.{}.1 dev lo".format(i, j, i, j + int(self.k / 2)))
                 print("pod-{}-agg-{}".format(i, j), "lo addr:", "15.{}.{}.1".format(i, j + int(self.k / 2)))
-            # Edge
-            for j in range(0, int(self.k / 2)):
-                os.system("docker exec pod-{}-edge-{} ip addr add 15.{}.{}.1 dev lo".format(i, j, i, j))
-                print("pod-{}-edge-{}".format(i, j), "lo addr:", "15.{}.{}.1".format(i, j))
             
-            # Host
-            host_id = 0
-            for j in range(int(self.k / 2)):
-                for h in range(2, int(self.k / 2) + 2):
-                    os.system("docker exec -it pod-{}-host-{} ip addr add 15.{}.{}.{} dev lo".format(i, host_id, i, j, h))
-                    print("pod-{}-host-{}".format(i, host_id), "lo addr:", "15.{}.{}.{}".format(i, j, h))
-                    host_id += 1
         print("finish assigning loopback interface ip addresses.")
 
 
@@ -133,8 +128,8 @@ class FatTree:
                 print("core-{}".format(core_id), "pid =", pid_core)
                 for pod in range(0, self.k):
                     pid_agg = sp.check_output(['docker', 'inspect', '-f', '{{.State.Pid}}', 'pod-{}-agg-{}'.format(pod, agg)]).decode("utf-8").strip()
-                    print("pod-{}-agg-{}".format(pod, agg), "pid =", pid_agg)
                     os.system("sudo ln -sfT /proc/{}/ns/net /var/run/netns/{}".format(pid_agg, pid_agg))
+                    print("pod-{}-agg-{}".format(pod, agg), "pid =", pid_agg)
                     
                     os.system("sudo ip link add ca-c-{}-p-{}-a-{} type veth peer name ca-p-{}-a-{}-c-{}".format(core_id, pod, agg, pod, agg, core_id))
                     os.system("sudo ip link set ca-c-{}-p-{}-a-{} netns {}".format(core_id, pod, agg, int(pid_core)))
@@ -145,9 +140,6 @@ class FatTree:
 
                     os.system('sudo ip -n {} link set dev ca-c-{}-p-{}-a-{} up'.format(int(pid_core), core_id, pod, agg))
                     os.system('sudo ip -n {} link set dev ca-p-{}-a-{}-c-{} up'.format(int(pid_agg), pod, agg, core_id))  
-                   
-                    print('sudo ip -n {} addr add {}.{}.{}.{}/8 dev ca-c-{}-p-{}-a-{}'.format(int(pid_core), 169, self.k + pod, agg, core_id, core_id, pod, agg))
-                    print('sudo ip -n {} addr add {}.{}.{}.{}/8 dev ca-p-{}-a-{}-c-{}'.format(int(pid_agg), 169, self.k + pod, agg + self.num_of_half_pod_sw, core_id, pod, agg, core_id))
 
                 core_id += 1
         print("finish setting up links between core switches and aggregation switches.")
@@ -170,8 +162,7 @@ class FatTree:
                     os.system('sudo ip -n {} link set dev ae-p-{}-a-{}-e-{} up'.format(int(pid_agg), pod, agg, edge))
                     os.system('sudo ip -n {} link set dev ae-p-{}-e-{}-a-{} up'.format(int(pid_edge), pod, edge, agg))
                     
-                    print('sudo ip -n {} addr add {}.{}.{}.{}/8 dev ae-p-{}-a-{}-e-{}'.format(int(pid_agg), 169, pod, agg + self.num_of_half_pod_sw, edge, pod, agg, edge))
-                    print('sudo ip -n {} addr add {}.{}.{}.{}/8 dev ae-p-{}-e-{}-a-{}'.format(int(pid_edge), 169, pod, edge, agg+self.num_of_half_pod_sw, pod, edge, agg))
+                 
 
         print("finish setting up links between aggregation switches and edge switches.")
 
@@ -181,22 +172,31 @@ class FatTree:
             for edge in range(0, self.num_of_half_pod_sw):
                 pid_edge = sp.check_output(['docker', 'inspect', '-f', '{{.State.Pid}}', 'pod-{}-edge-{}'.format(pod, edge)]).decode("utf-8").strip()
                 os.system("sudo ln -sfT /proc/{}/ns/net /var/run/netns/{}".format(pid_edge, pid_edge))
+
+                os.system("sudo brctl addbr br-p-{}-e-{}".format(pod, edge))
+                os.system("sudo ip link set dev br-p-{}-e-{} up".format(pod, edge))
+                os.system('sudo ip addr add {}.{}.{}.{}/24 dev br-p-{}-e-{}'.format(15, pod, edge, 1, pod, edge))
+                os.system("sudo ip link add vbr-p-{}-e-{} type veth peer name ve-p-{}-e-{}".format(pod, edge, pod, edge))
+                os.system("sudo ip link set vbr-p-{}-e-{} master br-p-{}-e-{}".format(pod, edge, pod, edge))
+                os.system("sudo ip link set dev vbr-p-{}-e-{} up".format(pod, edge))
+                os.system("sudo ip link set ve-p-{}-e-{} netns {}".format(pod, edge, int(pid_edge)))
+                os.system("sudo ip -n {} link set dev ve-p-{}-e-{} up".format(int(pid_edge), pod, edge))
+
                 for host in range(2, self.num_of_half_pod_sw + 2):
                     pid_host = sp.check_output(['docker', 'inspect', '-f', '{{.State.Pid}}', 'pod-{}-host-{}'.format(pod, host_id)]).decode("utf-8").strip()
                     os.system("sudo ln -sfT /proc/{}/ns/net /var/run/netns/{}".format(pid_host, pid_host))
+
                     os.system("sudo ip link add eh-p-{}-e-{}-h-{} type veth peer name eh-p-{}-h-{}-e-{}".format(pod, edge, host_id, pod, host_id, edge))
-                    os.system('sudo ip link set eh-p-{}-e-{}-h-{} netns {}'.format(pod, edge, host_id, int(pid_edge)))
+                    print("sudo ip link add eh-p-{}-e-{}-h-{} type veth peer name eh-p-{}-h-{}-e-{}".format(pod, edge, host_id, pod, host_id, edge))
+                    
+                    os.system('sudo ip link set eh-p-{}-e-{}-h-{} master br-p-{}-e-{}'.format(pod, edge, host_id, pod, edge))
                     os.system('sudo ip link set eh-p-{}-h-{}-e-{} netns {}'.format(pod, host_id, edge, int(pid_host)))
+                    
+                    os.system("sudo ip -n {} addr add {}.{}.{}.{}/24 dev eh-p-{}-h-{}-e-{}".format(int(pid_host), 15, pod, edge, host, pod, host_id, edge))
 
-                    os.system('sudo ip -n {} addr add {}.{}.{}.{}/8 dev eh-p-{}-e-{}-h-{}'.format(int(pid_edge), 169, pod, edge + self.k, host - 2, pod, edge, host_id))
-                    os.system("sudo ip -n {} addr add {}.{}.{}.{}/8 dev eh-p-{}-h-{}-e-{}".format(int(pid_host), 169, pod, edge + self.k + self.num_of_half_pod_sw, host-2, pod, host_id, edge))
-
-                    os.system('sudo ip -n {} link set dev eh-p-{}-e-{}-h-{} up'.format(int(pid_edge), pod, edge, host_id))
+                    os.system('sudo ip link set dev eh-p-{}-e-{}-h-{} up'.format(pod, edge, host_id))
                     os.system('sudo ip -n {} link set dev eh-p-{}-h-{}-e-{} up'.format(int(pid_host), pod, host_id, edge))
                     
-                    print('sudo ip -n {} addr add {}.{}.{}.{}/8 dev eh-p-{}-e-{}-h-{}'.format(int(pid_edge), 169, pod, edge + self.k, host - 2, pod, edge, host_id))
-                    print("sudo ip -n {} addr add {}.{}.{}.{}/8 dev eh-p-{}-h-{}-e-{}".format(int(pid_host), 169, pod, edge + self.k + self.num_of_half_pod_sw, host-2, pod, host_id, edge))
                     host_id += 1
 
         print("finish setting up links between edge switches and hosts.")
-                    
